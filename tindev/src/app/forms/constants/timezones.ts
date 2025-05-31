45fc9c59e251f5887f62900fb0204234b68021ab
Humza Butt <Humzab1711@hotmail.com>
2025-05-31 16:42:25 +0100

    Add TIMEZONES constant to provide a comprehensive list of timezones for user selection in forms

---
+++ tindev/src/app/forms/constants/timezones.ts
@@ -0,0 +1,49 @@
+export const TIMEZONES = [
+  // UTC/GMT
+  'UTC', 'GMT',
+
+  // Americas
+  'America/New_York', 'America/Chicago', 'America/Denver', 'America/Los_Angeles', 'America/Toronto',
+  'America/Vancouver', 'America/Sao_Paulo', 'America/Buenos_Aires', 'America/Mexico_City',
+  'America/Phoenix', 'America/Anchorage', 'America/Halifax', 'America/Winnipeg',
+  'America/Caracas', 'America/Lima', 'America/Bogota', 'America/Santiago',
+  'America/St_Johns', 'America/Godthab', 'America/Argentina/Buenos_Aires',
+  'America/Montevideo', 'America/Noronha', 'America/Scoresbysund',
+
+  // Europe
+  'Europe/London', 'Europe/Paris', 'Europe/Berlin', 'Europe/Moscow', 'Europe/Istanbul',
+  'Europe/Madrid', 'Europe/Rome', 'Europe/Amsterdam', 'Europe/Zurich', 'Europe/Stockholm',
+  'Europe/Vienna', 'Europe/Dublin', 'Europe/Helsinki', 'Europe/Copenhagen', 'Europe/Oslo',
+  'Europe/Warsaw', 'Europe/Budapest', 'Europe/Prague', 'Europe/Athens', 'Europe/Bucharest',
+
+  // Asia
+  'Asia/Dubai', 'Asia/Kolkata', 'Asia/Shanghai', 'Asia/Tokyo', 'Asia/Singapore', 'Asia/Hong_Kong',
+  'Asia/Bangkok', 'Asia/Seoul', 'Asia/Jakarta', 'Asia/Manila', 'Asia/Kuala_Lumpur',
+  'Asia/Dhaka', 'Asia/Karachi', 'Asia/Taipei', 'Asia/Riyadh', 'Asia/Tehran',
+  'Asia/Jerusalem', 'Asia/Kathmandu', 'Asia/Yangon', 'Asia/Colombo', 'Asia/Tashkent',
+  'Asia/Almaty', 'Asia/Baku', 'Asia/Yekaterinburg', 'Asia/Vladivostok', 'Asia/Kamchatka',
+  'Asia/Magadan', 'Asia/Sakhalin', 'Asia/Irkutsk', 'Asia/Krasnoyarsk', 'Asia/Novosibirsk',
+  'Asia/Omsk', 'Asia/Baghdad', 'Asia/Kuwait', 'Asia/Qatar', 'Asia/Bahrain',
+  'Asia/Kabul', 'Asia/Dushanbe', 'Asia/Ashgabat', 'Asia/Bishkek',
+
+  // Australia
+  'Australia/Sydney', 'Australia/Melbourne', 'Australia/Brisbane', 'Australia/Perth',
+  'Australia/Adelaide', 'Australia/Darwin', 'Australia/Hobart', 'Australia/Lord_Howe',
+
+  // Africa
+  'Africa/Johannesburg', 'Africa/Cairo', 'Africa/Lagos', 'Africa/Nairobi',
+  'Africa/Casablanca', 'Africa/Tunis', 'Africa/Algiers', 'Africa/Khartoum',
+  'Africa/Addis_Ababa', 'Africa/Dar_es_Salaam', 'Africa/Kampala',
+
+  // Pacific
+  'Pacific/Auckland', 'Pacific/Honolulu', 'Pacific/Fiji', 'Pacific/Guam',
+  'Pacific/Port_Moresby', 'Pacific/Tongatapu', 'Pacific/Samoa', 'Pacific/Tahiti',
+  'Pacific/Noumea', 'Pacific/Marquesas', 'Pacific/Gambier', 'Pacific/Pitcairn',
+  'Pacific/Easter', 'Pacific/Chatham', 'Pacific/Apia', 'Pacific/Kiritimati',
+
+  // Generic UTC offsets
+  'Etc/GMT+12', 'Etc/GMT+11', 'Etc/GMT+10', 'Etc/GMT+9', 'Etc/GMT+8', 'Etc/GMT+7',
+  'Etc/GMT+6', 'Etc/GMT+5', 'Etc/GMT+4', 'Etc/GMT+3', 'Etc/GMT+2', 'Etc/GMT+1',
+  'Etc/GMT-1', 'Etc/GMT-2', 'Etc/GMT-3', 'Etc/GMT-4', 'Etc/GMT-5', 'Etc/GMT-6',
+  'Etc/GMT-7', 'Etc/GMT-8', 'Etc/GMT-9', 'Etc/GMT-10', 'Etc/GMT-11', 'Etc/GMT-12'
+];
